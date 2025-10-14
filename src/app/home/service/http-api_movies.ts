a376d427671acaf0681eb6fde8e276a4000d6239
fouzy213 <chikouchefouzy@gmail.com>
2025-10-14 10:13:10 +0200

    fetch_series

---
+++ src/app/home/service/http-api_movies.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../../environment';
+import { Observable } from 'rxjs';
+
+interface Movie {
+  id: number;
+  title: string;
+  poster_path: string;
+  overview: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HttpApiMovies {
+  url: string = environment.apiUrl;
+  token: string = environment.apiToken;
+
+  constructor(private http: HttpClient) {}
+
+  fetchGenres(): Observable<{ genres: { id: number; name: string }[] }> {
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${this.token}`,
+      'Content-Type': 'application/json;charset=utf-8',
+    });
+
+    return this.http.get<{ genres: { id: number; name: string }[] }>(
+      `${this.url}/genre/movie/list?language=en-US`,
+      { headers }
+    );
+  }
+
+  fetchMoviesByGenre(genreId: number): Observable<{ results: any[] }> {
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${this.token}`,
+      'Content-Type': 'application/json;charset=utf-8',
+    });
+
+    return this.http.get<{ results: any[] }>(
+      `${this.url}/discover/movie?with_genres=${genreId}&language=en-US`,
+      { headers }
+    );
+  }
+}
--- src/app/movies/movies.ts
+++ src/app/movies/movies.ts
@@ -1,9 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpApiMovies } from '../home/service/http-api_movies';
+import { CommonModule } from '@angular/common';
+import { forkJoin } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-movies',
-  imports: [],
-  template: ` <p>movies works!</p> `,
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <h1>Films par genre</h1>
+    @for (genre of genresWithMovies; track genre.id) {
+    <section>
+      <h2>{{ genre.name }}</h2>
+      <ul class="movie-list">
+        @for (movie of genre.movies; track movie.id) {
+        <li>
+          <img
+            [src]="'https://media.themoviedb.org/t/p/w300' + movie.poster_path"
+            [alt]="movie.title"
+          />
+          <h4>{{ movie.title }}</h4>
+        </li>
+        }
+      </ul>
+    </section>
+    }
+  `,
   styleUrl: './movies.css',
 })
-export class Movies {}
+export class Movies implements OnInit {
+  genresWithMovies: { id: number; name: string; movies: any[] }[] = [];
+
+  constructor(private http: HttpApiMovies) {}
+
+  ngOnInit(): void {
+    this.http.fetchGenres().subscribe({
+      next: (data) => {
+        const genreRequests = data.genres.map((genre) =>
+          this.http.fetchMoviesByGenre(genre.id).pipe(
+            map((res) => ({
+              id: genre.id,
+              name: genre.name,
+              movies: res.results.slice(0, 5),
+            }))
+          )
+        );
+
+        forkJoin(genreRequests).subscribe({
+          next: (genresMovies) => {
+            this.genresWithMovies = genresMovies;
+            console.log('Films par genre :', this.genresWithMovies);
+          },
+          error: (err) => console.error(err),
+        });
+      },
+      error: (err) => console.error('Erreur genres:', err),
+    });
+  }
+}
