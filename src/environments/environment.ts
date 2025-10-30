0c7656aa5646b13dd9ea0c0bdb55af9447649139
fouzy213 <fouzy_69@hotmail.fr>
2025-10-30 22:09:19 +0100

    vercel_project_1.0.3

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://api.themoviedb.org/3',
+  apiToken: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzM2JhY2NiNGIyMzMzNTFkN2E3MjM4NTY0YzVjMjgyZiIsIm5iZiI6MTczMTA3NzQ4NC40MjYxNTcyLCJzdWIiOiI2NzJhMjRlZjQyNWJkY2QyYWNlMzMxMWQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.2exLdFhzU6MnRKW_pXnSvCSAJt5nP6KhMrgJ5Idb0TU'
+};
